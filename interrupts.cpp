027d21582045549927f4fd4a37ea5fdf5ccfedea
Brodie <brodie.j.macleod@gmail.com>
2025-09-30 12:17:25 -0400

    Untested: Added CPU Activity Simulation and SYSCALL simulation

--- interrupts.cpp
+++ interrupts.cpp
@@ -19,9 +19,10 @@ int main(int argc, char** argv) {
     std::string execution;  //!< string to accumulate the execution output
 
     /******************ADD YOUR VARIABLES HERE*************************/
-
-
-
+    int negligible_time = 1; //1ms
+    int context_save_time = 10; 
+    int ISR_runtime = 40;
+    int current_time = 0;
     /******************************************************************/
 
     //parse each line of the input trace file
@@ -29,9 +30,47 @@ int main(int argc, char** argv) {
         auto [activity, duration_intr] = parse_trace(trace);
 
         /******************ADD YOUR SIMULATION CODE HERE*************************/
+        if (activity == "CPU") 
+        {
+            execution += std::to_string(current_time) + ", " + std::to_string(duration_intr) + ", CPU burst\n";
+            current_time += duration_intr;
+        }
+        else if(activity == "SYSCALL")
+        {
+            //to kernal mode, save context, find vector, load ISR into PC
+            auto [inter_out, new_time] = intr_boilerplate(current_time,duration_intr,context_save_time,vectors);
+            execution += inter_out;
+            current_time = new_time;
 
+            //execution of ISR
+            execution += (std::to_string(current_time) + "," + std::to_string(ISR_runtime ) + ",Executing ISR for device " +std::to_string(duration_intr) + "\n");
+            current_time += ISR_runtime;
 
+            //device operation. find device operation time from device table
+            int opr_time = 0; 
+            if(duration_intr >(int)sizeof(delays))
+            {
+                std::cout<<"ERROR: DEVICE " + std::to_string(duration_intr) +  " NOT FOUND"<<std::endl;
+            }
+            else
+            {
+                opr_time = delays[duration_intr];
+            }
 
+            execution += std::to_string(current_time) + ", " + std::to_string(opr_time) + ", device " + std::to_string(duration_intr) + " I/O operation\n";
+            current_time += opr_time;
+
+            //IRET
+            execution += std::to_string(current_time) + ", "+  std::to_string(negligible_time) + ",IRET\n";
+            current_time += negligible_time;
+
+        }
+        else if(activity == "END_IO"){
+            continue; //left incomplete for this commit 
+        }
+        else {
+            execution += std::to_string(current_time) + ", 0, UNKOWN ACTIVITY";
+        }
         /************************************************************************/
 
     }
